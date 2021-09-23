059f7cddc5d033489bef428bb83ce31a9fa2be1a
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 19:46:02 +1200

    l

--- includes/dbh.php
+++ includes/dbh.php
@@ -9,15 +9,4 @@ $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
 
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
-} else {
-    echo "Ya connected bro!!";
-}
-
-$sql = "INSERT INTO OpenJobs (JobName, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus, JobDate, Destination)
-VALUES ('Pallets ex Booths', 'Pick Up', '12', '154674', '540', '13500', 'Planned', 'Thursday, 23 Sep', 'Kapiti')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Didn't Work";
 }
