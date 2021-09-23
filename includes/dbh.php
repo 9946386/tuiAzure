abfdb1c725d90653d8ff26a2e7008eff15a440b0
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 13:03:25 +1200

    Added new stylesheet and removed in page styles

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServerName = "tuiprojectserver.database.windows.net";
+$dbUserName = "tuiprojectadmin";
+$dbPassword = "TuiProjectPassword123";
+$dbName = "tui-project-db";
+
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
--- pages/webAddJob.php
+++ pages/webAddJob.php
@@ -1,3 +1,7 @@
+<?php
+    include_once '/includes/dbh.php';
+?>
+
 <!doctype html>
 <html lang="en">
     <head>
