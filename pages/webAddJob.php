f621a0b4400fbac6086dfcb994b7d1e9553d988d
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 14:00:47 +1200

    moved PHP include

--- pages/webAddJob.php
+++ pages/webAddJob.php
@@ -1,53 +1,60 @@
 <?php
-    include_once '/includes/dbh.php';
+include_once '/includes/dbh.php';
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/styles/myTheme.css"><link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="stylesheet" hred="/styles/addedStyles.css">
-        
-        <title>Web - Add Job</title>
-    </head>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/styles/myTheme.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" hred="/styles/addedStyles.css">
+
+    <title>Web - Add Job</title>
+</head>
+
 <body>
-    <?php require_once 'process.php'; ?>
+
+    <?php
+    include_once '/includes/dbh.php';
+    ?>
 
     <!-- Header -->
     <header class="w-100">
         <div class="container-fluid bg-primary top-0 h-auto">
             <a href="/index.html" class="btn">
-            <div class="row text-center">
-                <img src="/images/WebLogo.png" alt="Tui Logo" class="h-100 p-4 mx-auto" ></a>
-            </div>
+                <div class="row text-center">
+                    <img src="/images/WebLogo.png" alt="Tui Logo" class="h-100 p-4 mx-auto">
+            </a>
+        </div>
         </div>
 
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-light bg-secondary webNavBar" id="navbar_top">
             <div class="container-fluid ">
-            <div class="collapse navbar-collapse " id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item mx-5 ">
-                    <a class="nav-link active text-light" aria-current="page" href="/index.html">Daily Plan</a>
-                </li>
-                <li class="nav-item mx-5">
-                    <a class="nav-link text-light" href="/pages/webWeeklyPlan.html">Weekly Plan</a>
-                </li>
-                <li class="nav-item mx-5">
-                    <a class="nav-link text-light" href="/pages/webArchives.html">Archives</a>
-                </li>
-                <li class="nav-item mx-5">
-                    <a class="nav-link text-light" href="/pages/webReports.html">Reports</a>
-                </li>
-                </ul>
-            </div>
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item mx-5 ">
+                            <a class="nav-link active text-light" aria-current="page" href="/index.html">Daily Plan</a>
+                        </li>
+                        <li class="nav-item mx-5">
+                            <a class="nav-link text-light" href="/pages/webWeeklyPlan.html">Weekly Plan</a>
+                        </li>
+                        <li class="nav-item mx-5">
+                            <a class="nav-link text-light" href="/pages/webArchives.html">Archives</a>
+                        </li>
+                        <li class="nav-item mx-5">
+                            <a class="nav-link text-light" href="/pages/webReports.html">Reports</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
@@ -63,7 +70,7 @@
                     <a class="dropdown-item" href="#">Truck 2</a>
                     <a class="dropdown-item" href="#">Truck 3</a>
                     <a class="dropdown-item" href="#">Truck 4</a>
-                  </div>
+                </div>
             </div>
         </div>
     </div>
@@ -74,7 +81,7 @@
                 <div class="card-body">
                     <form action="process.php" method="POST">
                         <div class="row gy-5 form-group">
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Date:</label>
                             </div>
                             <div class="col-6">
@@ -82,7 +89,7 @@
                             </div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Destination:</label>
                             </div>
                             <div class="col-10">
@@ -90,13 +97,13 @@
                             </div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Type:</label>
                             </div>
                             <div class="col-4">
                                 <input type="text" class="form-control p-1" name="Type" value="Enter Type">
                             </div>
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Order #:</label>
                             </div>
                             <div class="col-4">
@@ -104,13 +111,13 @@
                             </div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Reference:</label>
                             </div>
                             <div class="col-4">
                                 <input type="text" class="form-control p-1" name="ReferenceNumber" value="Enter Reference">
                             </div>
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Pallets:</label>
                             </div>
                             <div class="col-4">
@@ -118,13 +125,13 @@
                             </div>
                         </div>
                         <div class="row form-group">
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Weight:</label>
                             </div>
                             <div class="col-4">
                                 <input type="text" class="form-control p-1" name="JobWeight" value="Enter Weight">
                             </div>
-                            <div class="col-2">  
+                            <div class="col-2">
                                 <label for="addJobDate" class="form-label p-1">Status:</label>
                             </div>
                             <div class="col-4">
@@ -138,10 +145,10 @@
                         </div>
                     </div>
                 </div>
-            </div>            
+            </div>
 
         </div>
-    </div> 
+    </div>
 
     <!-- Footer -->
     <div class="container-fluid bg-primary p-4">
@@ -183,4 +190,5 @@
 
 
 </body>
+
 </html>
