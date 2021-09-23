10a64100ddf8490d26e123edb05fe257907440db
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 21:19:13 +1200

    Add Job table

--- includes/insert.php
+++ includes/insert.php
@@ -1,6 +1,20 @@
 <?php
 
-include_once "dbh.php";
+// include_once "dbh.php";
+
+$dbServerName = "tuiprojectserver.database.windows.net";
+$dbUserName = "tuiprojectadmin";
+$dbPassword = "TuiProjectPassword123";
+$dbName = "tui-project-db";
+
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+    echo '<script>console.log("Failed Bro!")</script>';
+} else {
+    echo '<script>console.log("Success Bro!")</script>';
+}
 
 if (isset($_POST['submit'])) {
     $JobName = $_POST['JobName'];
