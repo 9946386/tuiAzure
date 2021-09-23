51cc524feacfa5bb1a2355102c59a07fd03204f1
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 20:07:01 +1200

    add job table

--- includes/insert.php
+++ includes/insert.php
@@ -2,13 +2,33 @@
 
 include "dbh.php";
 
-$sql = "INSERT INTO OpenJobs (JobName, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus, JobDate, Destination)
-VALUES ('Pallets ex Booths', 'Pick Up', '12', '154674', '540', '13500', 'Planned', 'Thursday, 23 Sep', 'Kapiti')";
+$JobName = $_POST['JobName'];
+$JobDate = $_POST['JobDate'];
+$Destination = $_POST['Destination'];
+$JobType = $_POST['JobType'];
+$OrderNumber = $_POST['OrderNumber'];
+$ReferenceNumber = $_POST['ReferenceNumber'];
+$Pallets = $_POST['Pallets'];
+$JobWeight = $_POST['JobWeight'];
+$JobStatus = $_POST['JobStatus'];
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+$sql = "INSERT INTO OpenJobs (JobName, JobDate, Destination, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus)
+VALUES ($JobName', '$JobDate', '$Destination', '$JobType', '$OrderNumber', '$ReferenceNumber', '$Pallets', '$JobWeight', '$JobStatus')";
 
-$conn->close();
+header("Location: ../index.php?");
+
+
+
+// Select Data to Display
+// $sql = "SELECT JobName, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus FROM OpenJobs";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     echo "<table><tr><th>JobType</th><th>OrderNumber</th></tr><tr><th>Reference</th><th>Pallets</th></tr><tr><th>Weight</th><th>Status</th></tr>";
+//     while ($row = $result->fetch_assoc()) {
+//         echo "<tr><td>" . $row["JobName"] . "</td><td>" . $row["OrderNumber"] . "</td><td>" . $row["ReferenceNumber"] . "</td><td>" . $row["Pallets"] . "</td><td>" . $row["JobWeight"] . "</td><td>" . $row["JobStatus"] . "</td></tr>";
+//     }
+//     echo "</table>";
+// } else {
+//     echo "0 results";
+// }
