c48c0a0c461e0ff7c2a20b4f2d6d5da2e4e4947f
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 14:20:19 +1200

    php

---
+++ pages/webHome.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/styles/myTheme.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="/styles/addedStyles.css">
+
+    <title>Web - Daily Plan</title>
+</head>
+
+<body>
+
+    <?php
+    include_once '/includes/dbh.php';
+    ?>
+
+    <?php
+    $sql = "SELECT * FROM Jobs;";
+    $results = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($results);
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['JobName'];
+        }
+    }
+    ?>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="/JS/app.js"></script>
+    <script src="/JS/ui.js"></script>
+
+
+</body>
+
+</html>
