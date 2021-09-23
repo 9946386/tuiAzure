993b3fe8570f242604e9c88b8c295610980f3144
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-23 19:31:13 +1200

    Test Insert data

--- includes/dbh.php
+++ includes/dbh.php
@@ -12,3 +12,15 @@ if ($conn->connect_error) {
 } else {
     echo "Ya connected bro!!";
 }
+
+
+$sql = "INSERT INTO OpenJobs (JobName, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus, JobDate, Destination)
+VALUES ('Pallets ex Booths', 'Pick Up', '12', '154674', '540', '13500', 'Planned', 'Thursday, 23 Sep', 'Kapiti')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
