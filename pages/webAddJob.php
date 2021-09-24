2799a7bcfeac14c328709e7710e5003064bb64de
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-24 21:13:09 +1200

    moved connection php

--- pages/webAddJob.php
+++ pages/webAddJob.php
@@ -1,3 +1,7 @@
+<?php
+include '/dbh.php';
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -17,7 +21,6 @@
 </head>
 
 <body>
-    <?php include '/includes/dbh.php'; ?>
     <!-- Header -->
     <header class="w-100">
         <div class="container-fluid bg-primary top-0 h-auto">
