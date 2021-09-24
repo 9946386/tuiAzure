2c4bac827d5c12f147f4fc543c5d61ddc5670694
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-24 21:34:24 +1200

    trying insert instead of dbh

--- includes/insert.php
+++ includes/insert.php
@@ -1,1 +1,35 @@
+<?php
 
+include 'dbh.php';
+
+// Check if form has been filled out by checking POST value
+if (isset($_POST['submit'])) {
+
+    // Save all $_POST values from form into seperate variables
+    $JobName = $_POST['JobName'];
+    $JobDate = $_POST['JobDate'];
+    $Destination = $_POST['Destination'];
+    $JobType = $_POST['JobType'];
+    $OrderNumber = $_POST['OrderNumber'];
+    $ReferenceNumber = $_POST['ReferenceNumber'];
+    $Pallets = $_POST['Pallets'];
+    $JobWeight = $_POST['JobWeight'];
+    $JobStatus = $_POST['JobStatus'];
+
+    $mysqli->query("INSERT INTO OpenJobs (JobName, JobDate, Destination, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus)
+    VALUES ('$JobName', '$JobDate', '$Destination', '$JobType', '$OrderNumber', '$ReferenceNumber', '$Pallets', '$JobWeight', '$JobStatus')") or die($mysqli->error);
+
+    // Create insert command
+    // $sql = "insert into OpenJobs(JobName, JobDate, Destination, JobType, OrderNumber, ReferenceNumber, Pallets, JobWeight, JobStatus)
+    // values ('$JobName', '$JobDate', '$Destination', '$JobType', '$OrderNumber', '$ReferenceNumber', '$Pallets', '$JobWeight', '$JobStatus')";
+
+    if ($connection->query($mysql) === TRUE) {
+        //include 'template/insert_header.php';
+        echo '<script>console.log("Success Bro!")</script>';
+        //include 'template/footer.php';
+    } else {
+        //include 'template/insert_header.php';
+        echo '<script>console.log("Failed Bro!")</script>';
+        //include 'template/footer.php';
+    }
+}
