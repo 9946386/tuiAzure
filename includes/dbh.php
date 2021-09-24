530b37516e9746deced1acf10d3d324ce4f18790
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-24 22:35:01 +1200

    trying insert instead of dbh

--- includes/dbh.php
+++ includes/dbh.php
@@ -22,3 +22,8 @@ try {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "tuiprojectadmin", "pwd" => "{your_password_here}", "Database" => "tui-project-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:tuiprojectserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
