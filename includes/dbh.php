d22fe712471d64783be9354d5853fb08b0757eb2
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-24 22:10:15 +1200

    trying insert instead of dbh

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,14 +1,22 @@
 <?php
 
-$dbServerName = "tuiprojectserver.database.windows.net";
-$dbUserName = "tuiprojectadmin";
-$dbPassword = "TuiProjectPassword123";
-$dbName = "tui-project-db";
+// $dbServerName = "tuiprojectserver.database.windows.net";
+// $dbUserName = "tuiprojectadmin";
+// $dbPassword = "TuiProjectPassword123";
+// $dbName = "tui-project-db";
 
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+// $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
 
-if (!$conn) {
-    echo '<script>console.log("Failed Bro!")</script>';
-} else {
-    echo '<script>console.log("Yes Bro!")</script>';
+// if (!$conn) {
+//     echo '<script>console.log("Failed Bro!")</script>';
+// } else {
+//     echo '<script>console.log("Yes Bro!")</script>';
+// }
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:tuiprojectserver.database.windows.net,1433; Database = tui-project-db", "tuiprojectadmin", "TuiProjectPassword123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
