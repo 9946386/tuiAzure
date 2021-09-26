24f32a220d684bb65e101b0c266a39bc8c4a608b
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-26 23:23:10 +1300

    L

--- connection.php
+++ connection.php
@@ -16,5 +16,6 @@ mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 //If connection failed, show the error
 if (mysqli_connect_errno()) 
 {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+    echo '<script>console.log("Success Bro!")</script>';
+// die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
