e301f403e5765ecf06befb6d2a6cbe84226f9e1c
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-26 23:29:58 +1300

    L

--- connection.php
+++ connection.php
@@ -11,11 +11,11 @@ $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 // Establish the connection
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
 if (mysqli_connect_errno()) 
 {
-    echo '<script>console.log("Success Bro!")</script>';
+    echo '<script>console.log("Failed!")</script>';
 // die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
