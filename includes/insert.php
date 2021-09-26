3c8163c114b2413009b6a5271576f3a2f6221ae8
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-26 23:13:15 +1300

    push to azure added connection

---
+++ includes/insert.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once '../connection.php';
+
+if (isset($_POST['submit'])) 
+{
+    $firstName = $_POST['JobName'];
+    $lastName = $_POST['JobDate'];
+    $sql = "INSERT INTO testTable (firstName, lastName)
+    VALUES ('$firstName', '$lastName')";
+    if (mysqli_query($conn, $sql)) {
+        echo '<script>console.log("Success Bro!")</script>';
+        echo "New record made";
+    }
+    else {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
