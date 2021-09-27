f2ae8efc2b09a9dbf20cecfc0186cdcc3e9eee63
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-27 21:37:54 +1300

    SSL

--- connection.php
+++ connection.php
@@ -8,13 +8,13 @@ $db_name = "tuiProjectDatabase";
 //Initializes MySQLi
 $conn = mysqli_init();
 
-mysqli_ssl_set($conn, NULL, NULL, "/SSL/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 //Establish the connection
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
-if ($mysqli->connect_error) 
+if (mysqli_connect_errno($conn)) 
 {
     echo '<script>console.log("Failed!")</script>';
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
