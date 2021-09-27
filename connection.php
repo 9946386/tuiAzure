6d45a43befd8aa0f870088cd3c6638c536e7b2b7
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-27 17:41:02 +1300

    SSL

--- connection.php
+++ connection.php
@@ -6,19 +6,19 @@ $password = 'tuiProjectPassword123';
 $db_name = 'tuiProjectDatabase';
 
 //Initializes MySQLi
-// $conn = mysqli_init();
+$conn = mysqli_init();
 
-// mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
-// Establish the connection
-// mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL);
+//Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 
 //If connection failed, show the error
-// if (mysqli_connect_errno()) 
-// {
-//     echo '<script>console.log("Failed!")</script>';
-//     die('Failed to connect to MySQL: ' . mysqli_connect_error());
-// }
+if (!$conn) 
+{
+    echo '<script>console.log("Failed!")</script>';
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
 
 // mysqli_ssl_set($conn, NULL, NULL, "/SSL/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
@@ -28,11 +28,11 @@ $db_name = 'tuiProjectDatabase';
 //     echo 'Connection error: ' . mysqli_connect_error();
 // }
 
-$conn = mysqli_init();
+// $conn = mysqli_init();
 
-mysqli_ssl_set($conn, NULL, NULL, "/SSL/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+// mysqli_ssl_set($conn, NULL, NULL, "/SSL/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 
-mysqli_real_connect($conn, 'tui-project-server.mysql.database.azure.com', 'impishorange4@tui-project-server', 'tuiProjectPassword123', 'tuiProjectDatabase', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+// mysqli_real_connect($conn, 'tui-project-server.mysql.database.azure.com', 'impishorange4@tui-project-server', 'tuiProjectPassword123', 'tuiProjectDatabase', 3306, MYSQLI_CLIENT_SSL);
+// if (mysqli_connect_errno($conn)) {
+//     die('Failed to connect to MySQL: ' . mysqli_connect_error());
+// }
