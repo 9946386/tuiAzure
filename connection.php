2eeace78552cc337f3d3a48485bb729aed04e825
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-27 20:25:34 +1300

    SSL

--- connection.php
+++ connection.php
@@ -1,35 +1,36 @@
 <?php
 
-$host = 'tui-project-server.mysql.database.azure.com';
-$username = 'impishorange4@tui-project-server';
-$password = 'tuiProjectPassword123';
-$db_name = 'tuiProjectDatabase';
+$host = "tui-project-server.mysql.database.azure.com";
+$username = "impishorange4@tui-project-server";
+$password = "tuiProjectPassword123";
+$db_name = "tuiProjectDatabase";
 
 //Initializes MySQLi
-// $conn = mysqli_init();
+$conn = mysqli_init();
 
-// mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "/SSL/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 //Establish the connection
-// mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 
 //If connection failed, show the error
-// if ($conn === false) 
-// {
-//     echo '<script>console.log("Failed!")</script>';
-//     die('Failed to connect to MySQL: ' . mysqli_connect_error());
-// }else{
-//     echo '<script>console.log("Connected!")</script>';
-// }
+if ($conn === false) 
+{
+    echo '<script>console.log("Failed!")</script>';
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+else {
+    echo '<script>console.log("Connected!")</script>';
+}
 
 // mysqli_ssl_set($conn, NULL, NULL, "/SSL/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
-$conn = mysqli_connect('tui-project-server.mysql.database.azure.com', 'impishorange4@tui-project-server', 'tuiProjectPassword123', 'tuiProjectDatabase', 3306);
+// $conn = mysqli_connect('tui-project-server.mysql.database.azure.com', 'impishorange4@tui-project-server', 'tuiProjectPassword123', 'tuiProjectDatabase', 3306);
 
-if (!$conn) {
-    echo '<script>console.log("Failed!")</script>';
-    echo 'Connection error: ' . mysqli_connect_error();
-}
+// if (!$conn) {
+//     echo '<script>console.log("Failed!")</script>';
+//     echo 'Connection error: ' . mysqli_connect_error();
+// }
 
 // $conn = mysqli_init();
 
