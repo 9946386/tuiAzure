d0756225c60953fa22a09e9d7fedcddb21edc39e
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-28 17:17:45 +1300

    PHPMyAdmin connect

--- includes/insert.php
+++ includes/insert.php
@@ -4,10 +4,18 @@ include '../local-db-connection.php';
 
 if (isset($_POST['submit'])) 
 {
-    $firstName = $_POST['JobName'];
-    $lastName = $_POST['JobDate'];
-    $sql = "INSERT INTO openJobs (jobName, jobType)
-    VALUES ('$firstName', '$lastName')";
+    $jobName = $_POST['jobName'];
+    $jobDate = $_POST['jobDate'];
+    $destination = $_POST['destination'];
+    $jobType = $_POST['jobType'];
+    $jobReference = $_POST['jobReference'];
+    $jobWeight = $_POST['jobWeight'];
+    $orderNumber = $_POST['orderNumber'];
+    $pallets = $_POST['pallets'];
+    $jobStatus = $_POST['jobStatus'];
+
+    $sql = "INSERT INTO `openjobs` (`ID`, `jobName`, `jobDate`, `destination`, `jobType`, `orderNumber`, `refereneceNumber`, `pallets`, `jobWeight`, `jobStatus`) 
+    VALUES (NULL, $jobName, $jobDate, $destination, $jobType, $jobReference, $jobWeight, $orderNumber, $pallets, $jobStatus);";
 
     if ($conn->query($sql) === TRUE) {
         include '../index.php';
