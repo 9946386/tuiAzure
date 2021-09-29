e6af4ff7366472c242c09c31068d8b5d9ca73b3f
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-30 09:18:58 +1300

    Added insert openjob function

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
         <div class="container px-4 p-3 webDailyPlanTruckCard vh-100">
             <div class="row gy-2">
                 <div class="col-12">
-                 <?php include 'includes/indexFunction.php'; openJobsList(); ?>
+                 
                 </div>  
             </div> 
         </div>   
