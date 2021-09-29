425fa3443336255b5b816893ec8098d7a7cd7012
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-30 12:49:47 +1300

    Index php funtions

--- includes/indexFunction.php
+++ includes/indexFunction.php
@@ -36,7 +36,7 @@ function openJobsList()
                 <div class='card-body'>
                     <div class='row justify-content-between'>
                         <div class='col-11'>
-                            <h5 class='card-title'>Driver:" . $id;
+                            <h5 class='card-title'>Driver:" . $i;
         echo "</h5>
                         </div>
                         <div class='col-1'>
