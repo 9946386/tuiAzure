77ade7755d9bf4c2257e43ee6393dbb26c0f24b2
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-30 09:51:04 +1300

    Index php funtions

--- index.php
+++ index.php
@@ -22,39 +22,4 @@
         </div>   
     </div>                                   
 
-    <!-- Footer -->
-    <div class="container-fluid bg-primary p-4">
-        <footer class="webFooter">
-            <div class="row w-75 m-auto align-items-center">
-                <div class="col-3 text-center">
-                    <a href="/index.php">
-                        <img src="/images/navLogo.png" alt="footerImg">
-                    </a>
-                </div>
-                <div class="col-6">
-                    <div class="row">
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/index.php">Daily Plan</a>
-                        </div>
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/pages/webWeeklyPlan.php">Weekly Plan</a>
-                        </div>
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/pages/webArchive.php">Archive</a>
-                        </div>
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/pages/webReports.php">Reports</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- <div class="col-3 text-center loginBtn m-auto">
-                    <button class="btn btn-secondary rounded-pill">Login to Truck App</button>
-                </div> -->
-                <div class="col-3 text-center loginBtn m-auto">
-                    <a href="/pages/mobileHome.html" class="btn btn-secondary rounded-pill">Login to Truck App</a>
-                </div>
-            </div>
-        </footer>
-    </div>
-
     <?php include 'footer.php' ?>
