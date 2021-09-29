0337990a68fcdee3d1a2f51163c8d7ded2257ff3
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-30 12:16:37 +1300

    Index php funtions

--- includes/indexFunction.php
+++ includes/indexFunction.php
@@ -36,7 +36,7 @@ function openJobsList()
                 <div class='card-body'>
                     <div class='row justify-content-between'>
                         <div class='col-11'>
-                            <h5 class='card-title'>{$id}</h5>
+                            <h5 class='card-title'>Driver: . {$id}</h5>
                         </div>
                         <div class='col-1'>
                             <a href='pages/webAddJob.php' class='btn btn-primary btn-sm text-light rounded-pill'>Add Job</a>
