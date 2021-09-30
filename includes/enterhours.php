6bcf294edacfbb1b324655ac258a5d0cac1c3e92
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-30 19:35:25 +1300

    Convert HTML pages to PHP

--- includes/enterhours.php
+++ includes/enterhours.php
@@ -12,7 +12,7 @@ if (isset($_POST['submit']))
     $mood = $_POST['mood'];
 
     // SQL query to insert the above variables into the openjobs table.
-    $sql = "INSERT INTO driverhours(diselLitres, driverHours, kms, mood) 
+    $sql = "INSERT INTO driverhours(dieselLitres, driverHours, kms, mood) 
     VALUES ('$diesel', '$hours', '$kms', '$mood');";
 
     $run = mysqli_query($conn, $sql) or die(mysqli_error($conn));
