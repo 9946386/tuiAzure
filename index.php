f8d0908853280e7ea610a245089b4fa20a4dc940
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-04 19:43:13 +1300

    Add job dropdown

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <div class="container px-4 p-3 webDailyPlanTruckCard">
             <div class="row gy-2">
                 <div class="col-12">
-                 <?php include 'includes/indexFunction.php'; openJobsList2(); ?>
+                 <?php include 'includes/indexFunction.php'; openJobsList(); ?>
                 </div>  
             </div> 
         </div>   
