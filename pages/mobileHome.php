15ab329a189e32041d24fb3ca835b53102915f6b
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-04 19:59:37 +1300

    JavaScript issues

--- pages/mobileHome.php
+++ pages/mobileHome.php
@@ -55,7 +55,7 @@
         </div>
     </div>
 
-         <!-- Bootstrap Bundle with Popper -->
+    <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     <script src="../JS/app.js"></script>
     <script src="../JS/ui.js"></script>
