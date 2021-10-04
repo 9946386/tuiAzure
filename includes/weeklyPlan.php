f98156d7609c4afbf31730e7c0b9631cd8404885
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-04 21:40:07 +1300

    Getting days of jobs

--- includes/weeklyPlan.php
+++ includes/weeklyPlan.php
@@ -9,25 +9,24 @@ function monday()
                                     FROM openjobs
                                     INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
                                     WHERE weekday(jobDate) = 0");
+    echo "<div class='row'>
+        <div class='col pt-3'>
+            <table class='table table-bordered table-responsive'>
+                <thead>
+                <tr class='table-light'>
+                        <th scope='col' class='col-2'>Job</th>
+                        <th scope='col'>Type</th>
+                        <th scope='col' class='col-2'>Order #</th>
+                        <th scope='col' class='col-2'>Reference</th>
+                        <th scope='col'>Pallets</th>
+                        <th scope='col'>Weight (kg)</th>
+                        <th scope='col' class='col-2'>Status</th>
+                    </tr>
+                </thead>";
+
     while ($row = mysqli_fetch_assoc($query)) {
         //$id = $row['DriverID'];
         //$driverName_fk = $row['driverName_fk'];
-
-        echo "<div class='row'>
-                        <div class='col pt-3'>
-                            <table class='table table-bordered table-responsive'>
-                                <thead>
-                                 <tr class='table-light'>
-                                        <th scope='col' class='col-2'>Job</th>
-                                        <th scope='col'>Type</th>
-                                        <th scope='col' class='col-2'>Order #</th>
-                                        <th scope='col' class='col-2'>Reference</th>
-                                        <th scope='col'>Pallets</th>
-                                        <th scope='col'>Weight (kg)</th>
-                                        <th scope='col' class='col-2'>Status</th>
-                                    </tr>
-                                </thead>";
-
         $jobName = $row['jobName'];
         $jobType = $row['jobType'];
         $orderNumber = $row['orderNumber'];
@@ -36,21 +35,19 @@ function monday()
         $jobWeight = $row['jobWeight'];
         $jobStatus = $row['jobStatus'];
 
-        echo "              <tr>
-                                    <th>{$jobName}</th>
-                                    <td>{$jobType}</td>
-                                    <td>{$orderNumber}</td>
-                                    <td>{$referenceNumber}</td>
-                                    <td>{$pallets}</td>
-                                    <td>{$jobWeight}</td>
-                                    <td>{$jobStatus}</td>
-                                </tr> ";
-
-
-        echo "</table>
-                        </div>
-                    </div>
+        echo "<tr>
+                                <th>{$jobName}</th>
+                                <td>{$jobType}</td>
+                                <td>{$orderNumber}</td>
+                                <td>{$referenceNumber}</td>
+                                <td>{$pallets}</td>
+                                <td>{$jobWeight}</td>
+                                <td>{$jobStatus}</td>
+                            </tr> 
+                    </table>
                 </div>
+            </div>
+            </div>
             </div>";
     }
 }
