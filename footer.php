f4662a8fb0d4f60bd463a072c1259d837071d93f
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-04 19:45:35 +1300

    Add job dropdown

--- footer.php
+++ footer.php
@@ -1,31 +1,31 @@
 <!-- Footer -->
 <div class="container-fluid bg-primary p-4">
-        <footer class="webFooter">
-            <div class="row w-75 m-auto align-items-center">
-                <div class="col-3 text-center">
-                    <a href="/index.html">
-                        <img src="/images/navLogo.png" alt="footerImg">
-                    </a>
-                </div>
-                <div class="col-6">
-                    <div class="row">
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/index.html">Daily Plan</a>
-                        </div>
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/pages/webWeeklyPlan.html">Weekly Plan</a>
-                        </div>
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/pages/webArchive.html">Archive</a>
-                        </div>
-                        <div class="col text-center">
-                            <a class="text-light m-auto" role="button" href="/pages/webReports.html">Reports</a>
-                        </div>
+    <footer class="webFooter">
+        <div class="row w-75 m-auto align-items-center">
+            <div class="col-3 text-center">
+                <a href="/index.html">
+                    <img src="/images/navLogo.png" alt="footerImg">
+                </a>
+            </div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col text-center">
+                        <a class="text-light m-auto" role="button" href="/index.html">Daily Plan</a>
+                    </div>
+                    <div class="col text-center">
+                        <a class="text-light m-auto" role="button" href="/pages/webWeeklyPlan.html">Weekly Plan</a>
+                    </div>
+                    <div class="col text-center">
+                        <a class="text-light m-auto" role="button" href="/pages/webArchive.html">Archive</a>
+                    </div>
+                    <div class="col text-center">
+                        <a class="text-light m-auto" role="button" href="/pages/webReports.html">Reports</a>
                     </div>
                 </div>
-                <div class="col-3 text-center loginBtn m-auto">
-                    <button class="btn btn-secondary rounded-pill">Login to Truck App</button>
-                </div>
             </div>
-        </footer>
-    </div>
+            <div class="col-3 text-center loginBtn m-auto">
+                <button class="btn btn-secondary rounded-pill">Login to Truck App</button>
+            </div>
+        </div>
+    </footer>
+</div>
