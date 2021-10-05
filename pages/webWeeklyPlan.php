f3686da4303308f36af2fa55e7abc24b0871490d
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-05 17:54:51 +1300

    Getting days of jobs

--- pages/webWeeklyPlan.php
+++ pages/webWeeklyPlan.php
@@ -5,71 +5,70 @@
 <?php include '../header.php' ?>
 
 <!-- Page Title -->
-<div class="container-sm text-dark px-3 p-4 truckList">
+    <div class="container-sm text-dark px-3 p-4 truckList">
         <div class="row m-auto align-items-center">
             <?php include '../includes/functions.php'; driverMenu();?>
         </div>
     </div>
 
+    <?php 
+        global $conn;
+        $query = mysqli_query($conn, "SELECT *
+                                        FROM openjobs
+                                        INNER JOIN driver ON openjobs.driverName_fk = driver.driverName
+                                        WHERE weekday(jobDate) = 0");
+
+        $tuesday = mysqli_query($conn, "SELECT *
+                                            FROM openjobs
+                                            INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
+                                            WHERE weekday(jobDate) = 1");
+
+        $wednesday = mysqli_query($conn, "SELECT *
+                                            FROM openjobs
+                                            INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
+                                            WHERE weekday(jobDate) = 2");
+
+        $thursday = mysqli_query($conn, "SELECT *
+                                            FROM openjobs
+                                            INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
+                                            WHERE weekday(jobDate) = 3");
+
+        $friday = mysqli_query($conn, "SELECT *
+                                        FROM openjobs
+                                        INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
+                                        WHERE weekday(jobDate) = 4");
+    ?>
+
     <!-- Truck's Weekly Job List -->
     <div class="container-fluid bg-secondary darkContainer">
-        <div class="container py-5 px-4 p-3 webWeeklyPlanTruckCard ">
+        <div class="container py-5 px-4 p-3 webWeeklyPlanTruckCard">
             <div class="row gy-2"> 
-                <div class="col-12">
-
-                <?php 
-                    global $conn;
-                    $query = mysqli_query($conn, "SELECT *
-                                                    FROM openjobs
-                                                    INNER JOIN driver ON openjobs.driverName_fk = driver.driverName
-                                                    WHERE weekday(jobDate) = 0");
-
-                    $tuesday = mysqli_query($conn, "SELECT *
-                                                        FROM openjobs
-                                                        INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
-                                                        WHERE weekday(jobDate) = 1");
-
-                    $wednesday = mysqli_query($conn, "SELECT *
-                                                        FROM openjobs
-                                                        INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
-                                                        WHERE weekday(jobDate) = 2");
-
-                    $thursday = mysqli_query($conn, "SELECT *
-                                                        FROM openjobs
-                                                        INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
-                                                        WHERE weekday(jobDate) = 3");
-
-                    $friday = mysqli_query($conn, "SELECT *
-                                                    FROM openjobs
-                                                    INNER JOIN driver ON openjobs.driver_fk = driver.DriverID
-                                                    WHERE weekday(jobDate) = 4");
-                ?>
-
+                <div class="col-12">               
 
                     <!-- Monday -->
-                    <div class='card mondayJobCard my-1'>
-                        <div class='card-body'>
-                            <div class='row justify-content-between'>
-                                <div class='col-11'>
-                                    <h5 class='card-title'>Monday ...</h5>
+                    <div class="card mondayJobCard my-1">
+                        <div class="card-body">
+                            <div class="row justify-content-between">
+                                <div class="col-11">
+                                    <h5 class="card-title">Monday ...</h5>
                                 </div>
-                                <div class='col-1'>                            
-                                    <a href='/pages/webAddJob.html' class='btn btn-primary btn-sm text-light rounded-pill'>Add Job</a>
+                                <div class="col-1">                            
+                                    <a href="/pages/webAddJob.html" class="btn btn-primary btn-sm text-light rounded-pill">Add Job</a>
                                 </div>
                             </div>
-                            <div class='row'>
-                                <div class='col pt-3'>
-                                    <table class='table table-bordered table-responsive'>
+                            <div class="row">
+                                <div class="col pt-3">
+                                    <table class="table table-bordered table-responsive">
                                         <thead>
-                                        <tr class='table-light'>
-                                                <th scope='col' class='col-2'>Job</th>
-                                                <th scope='col'>Name</th>
-                                                <th scope='col'>Type</th>
-                                                <th scope='col' class='col-2'>Order #</th>
-                                                <th scope='col' class='col-2'>Reference</th>
-                                                <th scope='col'>Pallets</th>
-                                                <th scope='col'>Weight (kg)</th>
-                                                <th scope='col' class='col-2'>Status</th>
+                                            <tr class="table-light">
+                                                <th scope="col" class="col-2">Job</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Type</th>
+                                                <th scope="col" class="col-2">Order #</th>
+                                                <th scope="col" class="col-2">Reference</th>
+                                                <th scope="col">Pallets</th>
+                                                <th scope="col">Weight (kg)</th>
+                                                <th scope="col" class="col-2">Status</th>
                                             </tr>
                                         </thead>
 
@@ -116,19 +115,19 @@
                                     <a href="/pages/webAddJob.html" class="btn btn-primary btn-sm text-light rounded-pill">Add Job</a>
                                 </div>
                             </div>
-                            <div class='row'>
-                                <div class='col pt-3'>
-                                    <table class='table table-bordered table-responsive'>
+                            <div class="row">
+                                <div class="col pt-3">
+                                    <table class="table table-bordered table-responsive">
                                         <thead>
-                                            <tr class='table-light'>
-                                                <th scope='col' class='col-2'>Job</th>
-                                                <th scope='col'>Name</th>
-                                                <th scope='col'>Type</th>
-                                                <th scope='col' class='col-2'>Order #</th>
-                                                <th scope='col' class='col-2'>Reference</th>
-                                                <th scope='col'>Pallets</th>
-                                                <th scope='col'>Weight (kg)</th>
-                                                <th scope='col' class='col-2'>Status</th>
+                                            <tr class="table-light">
+                                                <th scope="col" class="col-2">Job</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Type</th>
+                                                <th scope="col" class="col-2">Order #</th>
+                                                <th scope="col" class="col-2">Reference</th>
+                                                <th scope="col">Pallets</th>
+                                                <th scope="col">Weight (kg)</th>
+                                                <th scope="col" class="col-2">Status</th>
                                             </tr>
                                         </thead>
 
@@ -232,7 +231,7 @@
                             </div>
                             <div class="row">
                                 <div class="col pt-3">
-                                    <table class="table table-bordered table-responsive ">
+                                    <table class="table table-bordered table-responsive">
                                         <thead>
                                             <tr class="table-light">
                                                 <th scope="col" class="col-2">Job</th>
@@ -274,6 +273,7 @@
                             </div>
                         </div>
                     </div>
+                    
                     <!-- Friday -->
                     <div class="card fridayJobCard my-2">
                         <div class="card-body">
@@ -287,7 +287,7 @@
                             </div>
                             <div class="row">
                                 <div class="col pt-3">
-                                    <table class="table table-bordered table-responsive ">
+                                    <table class="table table-bordered table-responsive">
                                         <thead>
                                             <tr class="table-light">
                                                 <th scope="col" class="col-2">Job</th>
