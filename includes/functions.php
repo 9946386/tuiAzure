f341a8b1e45feee2b2475b184c8c9ae1a96ead60
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-05 18:37:25 +1300

    Getting days of jobs

--- includes/functions.php
+++ includes/functions.php
@@ -14,7 +14,7 @@ function driverMenu()
         $name = $row['driverName'];
 
         echo "<div class='col text-center'>
-                <a href='webWeeklyPlan.php?id={$id}' name='driverNameBtn' class='btn btn-primary rounded-pill text-light'>{$name}</a>
+                <input type='submit' href='webWeeklyPlan.php?id={$id}' name='driverNameBtn' class='btn btn-primary rounded-pill text-light'>{$name}</input>
                 </div>";
     }
 }
