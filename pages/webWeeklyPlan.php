47df3c97e34df66155acaad25fa0625729c9675e
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-06 20:36:31 +1300

    Getting days of jobs

--- pages/webWeeklyPlan.php
+++ pages/webWeeklyPlan.php
@@ -66,7 +66,7 @@
                 
                 
             while( $row = $results->fetch_object() ) {
-                if( (int)date( 'w', strtotime( $row->jobDate ) ) == $days){
+                if( (int)date( 'w', strtotime( $row->jobDate ) ) == $i){
                     printf('<tbody>
                                 <tr data-did="%9$s" data-driver="%1$s">
                                     <th>%2$s</th>
