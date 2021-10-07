497cace69c3e80bdddda549c5bc2ae0df6425b9a
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-07 17:20:35 +1300

    Getting days of jobs

--- pages/mobileHome.php
+++ pages/mobileHome.php
@@ -6,11 +6,7 @@
 <?php 
 session_start();
 include '../local-db-connection.php'; 
-
-$name = $_SESSION["name"];
-
-$sql = mysqli_query($conn, "SELECT * FROM openJobs WHERE driverName_fk = $name");
-$results = $conn->query( $sql ); ?>
+?>
 
 <!-- Page Title -->
     <div class="container-sm text-dark px-3 p-2">
@@ -30,57 +26,72 @@ $results = $conn->query( $sql ); ?>
                       
                           <?php
 
-while ($row = mysqli_fetch_assoc($sql)) {
-  $jobName = $row['jobName'];
-  $jobType = $row['jobType'];
-  $orderNumber = $row['orderNumber'];
-  $referenceNumber = $row['referenceNumber'];
-  $pallets = $row['pallets'];
-  $jobWeight = $row['jobWeight'];
-  $jobStatus = $row['jobStatus'];
+if(!isset($_SESSION['useruid'])){
+  echo "You are not logged in";
+}
+else{
+  $sql = mysqli_query($conn, "SELECT usersID FROM users 
+                              WHERE userID = '" . $_SESSION['useruid'] ."'"); 
 
-  echo "              <h5 class='card-title'>{$jobName}</h5>
-                      <table class='table table-responsive'>
-                        <tbody>
-                          <tr>
-                            <th>Type:</th>
-                            <td>{$jobType}}</td>
-                          </tr>
-                          <tr>
-                            <th>Order #:</th>
-                            <td>{$orderNumber}</td>
-                          </tr>
-                          <tr>
-                            <th>Reference:</th>
-                            <td>{$referenceNumber}</td>
-                          </tr>
-                          <tr>
-                            <th>Pallets:</th>
-                            <td>{$pallets}</td>
-                          </tr>
-                          <tr>
-                            <th>Weight:</th>
-                            <td>{$jobWeight}</td>
-                          </tr>
-                          <tr>
-                            <th>Status:</th>
-                            <td>{$jobStatus}</td>
-                          </tr> ";  
+                              $userID = $row['userID'];
+
+  echo "User with ID: {$userID} is logged in";
+  echo "2. User with ID: {$_SESSION['useruid']} is logged in";
+
+  // while ($row = mysqli_fetch_assoc($sql)) {
+  //   $jobName = $row['jobName'];
+  //   $jobType = $row['jobType'];
+  //   $orderNumber = $row['orderNumber'];
+  //   $referenceNumber = $row['referenceNumber'];
+  //   $pallets = $row['pallets'];
+  //   $jobWeight = $row['jobWeight'];
+  //   $jobStatus = $row['jobStatus'];
+  
+  //   echo "              <h5 class='card-title'>{$jobName}</h5>
+  //                       <table class='table table-responsive'>
+  //                         <tbody>
+  //                           <tr>
+  //                             <th>Type:</th>
+  //                             <td>{$jobType}}</td>
+  //                           </tr>
+  //                           <tr>
+  //                             <th>Order #:</th>
+  //                             <td>{$orderNumber}</td>
+  //                           </tr>
+  //                           <tr>
+  //                             <th>Reference:</th>
+  //                             <td>{$referenceNumber}</td>
+  //                           </tr>
+  //                           <tr>
+  //                             <th>Pallets:</th>
+  //                             <td>{$pallets}</td>
+  //                           </tr>
+  //                           <tr>
+  //                             <th>Weight:</th>
+  //                             <td>{$jobWeight}</td>
+  //                           </tr>
+  //                           <tr>
+  //                             <th>Status:</th>
+  //                             <td>{$jobStatus}</td>
+  //                           </tr>";
+  
                           
-                          }?>
+  //   }
+  //   echo "</tbody>                          
+  //   </table>
+  //   <div class='row'>
+  //     <div class='col d-flex flex-row-reverse'>
+  //       <a href='jobDetails.php' class='btn btn-primary text-light btn-sm'>View Job</a>
+  //     </div>
+  //   </div>
+  // </div>
+  // </div>
+  // </div>
+  // </div>
+  // </div>";
+  } ?>
 
-                        </tbody>                          
-                      </table>
-                      <div class="row">
-                        <div class="col d-flex flex-row-reverse">
-                          <a href="jobDetails.php" class="btn btn-primary text-light btn-sm">View Job</a>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-            </div>
-        </div>
-    </div>
+
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
