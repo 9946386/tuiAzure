46469f07d86d34f2d0bbec2ce501edf53ee39133
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-07 16:32:44 +1300

    Getting days of jobs

--- pages/mobileHome.php
+++ pages/mobileHome.php
@@ -1,3 +1,6 @@
+<?php ini_set('error_reporting', E_ALL); ?>
+<?php ini_set('display_errors', 1); ?>
+<?php ini_set('display_startup_errors', 1); ?>
 <?php include '../includes/mobileHeader.php' ?>
 
 <?php 
