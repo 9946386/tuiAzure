55f4027aef00a84eb0beefe8f5df9c848b450f96
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-09 11:43:18 +1300

    Change mobile nav depending on sign in

--- includes/indexFunction.php
+++ includes/indexFunction.php
@@ -5,20 +5,20 @@ include 'local-db-connection.php';
 function openJobsList()
 {
     global $conn;
-    $query = mysqli_query($conn, "SELECT users.usersID users.userName
+    $query = mysqli_query($conn, "SELECT users.usersID, users.userName
                                     FROM openjobs
                                     INNER JOIN users ON openjobs.driver_fk = users.usersID");
 
     while ($row = mysqli_fetch_assoc($query)) {
 
         $id = $row['usersID'];
-        //$driverName_fk = $row['driverName'];
+        $driverName_fk = $row['userName'];
 
         echo "<div class='card mainPageJobCard my-2'>
                 <div class='card-body'>
                     <div class='row justify-content-between'>
                         <div class='col-11'>
-                            <h5 class='card-title'>Driver: {$id}</h5>
+                            <h5 class='card-title'>Driver: {$id} {$driverName_fk}</h5>
                         </div>
                         <div class='col-1'>
                             <a href='../pages/webAddJob.php' class='btn btn-primary btn-sm text-light rounded-pill'>Add Job</a>
@@ -42,7 +42,7 @@ function openJobsList()
         $openjobq = mysqli_query($conn, "SELECT openjobs.jobName, openjobs.jobType, openjobs.orderNumber, openjobs.referenceNumber, openjobs.pallets, openjobs.jobWeight, openjobs.jobStatus, users.usersID, users.userName
                                             FROM openjobs
                                             INNER JOIN users ON openjobs.driver_fk = users.usersID
-                                            WHERE user.usersID = $id");
+                                            WHERE users.usersID = $id");
 
         while ($row = mysqli_fetch_assoc($openjobq)) {
             $jobName = $row['jobName'];
