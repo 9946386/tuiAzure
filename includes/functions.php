4e31bd5675b086888bce95130da77c4c6fa87127
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-10 20:52:59 +1300

    Sticky header after scroll

--- includes/functions.php
+++ includes/functions.php
@@ -187,7 +187,7 @@ function getUserJobs()
                                     INNER JOIN openjobs ON users.usersID = openjobs.driver_fk
                                     WHERE users.usersID = '" . $_SESSION['userid'] . "' ");
 
-        while ($row = mysqli_fetch_assoc($sql)) {
+        while ($row = mysqli_fetch_row($sql)) {
 
             // Assigning variables to use when fetching allocated jobs
             $id = $row['usersID'];
