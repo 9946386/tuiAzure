0d12fb02668bdb7cf154c139062af6effb4388a7
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-13 19:42:30 +1300

    Fixing search

--- pages/webArchives.php
+++ pages/webArchives.php
@@ -57,7 +57,7 @@
                         <?php if (isset($_POST['submit'])) {
 
                             global $conn;
-                            $search = mysqli_real_escape_string($conn, $_POST['search']);
+                            $search = $_POST['search'];
 
                             // Select all from completed jobs where the reference or order number is what was entered in the search input
                             $sql = "SELECT * FROM completedJobs WHERE completedReferenceNumber LIKE '%$search%' OR completedOrderNumber LIKE '%$search%'";
