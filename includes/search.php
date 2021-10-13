d4fadd9416e43a4ea8c54a3c701fc2924a70a768
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-13 18:47:57 +1300

    Fixing search

--- includes/search.php
+++ includes/search.php
@@ -58,7 +58,7 @@ include '../header.php' ?>
 if (isset($_POST['submit'])) {
 
     global $conn;
-    $search = mysqli_real_escape_string($conn, $_POST['search']);
+    $search = mysqli_real_escape_string($conn, $_POST['submit']);
 
     // Select all from completed jobs where the reference or order number is what was entered in the search input
     $sql = "SELECT * FROM completedJobs WHERE completedReferenceNumber LIKE '%$search%' OR completedOrderNumber LIKE '%$search%'";
