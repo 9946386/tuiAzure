a59a785253cec3b9af26e2e9abf82009671857e9
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-19 14:27:34 +1300

    Customer signnature to database

--- pages/webReports.php
+++ pages/webReports.php
@@ -4,63 +4,109 @@
 <?php ini_set('display_startup_errors', 1); ?>
 
 <!-- Including the header at the top of the page -->
-<?php include '../header.php' ?>
+<?php 
+include '../local-db-connection.php';
+include '../header.php' ?>
 
 <!-- Form that uses the search.php file to search database for anything that matches user input -->
-<form action="../includes/search.php" method="POST">
+
     <div class="container-sm text-dark px-3 p-4 searchInputs w-50">
+    <form action="" method="POST">
         <div class="row m-auto align-items-center">            
             <div class="col gy-3">
                 <div class="row gx-3">
                     <h6 class="p-2">Con Note:</h6>
                     <div class="input-group">
-                        <input type="text" class="form-control rounded-pill" placeholder="Search" name="search">
-                        <div class="input-group-btn">
-                            <button class="btn btn-default" type="submit" name="submit">
-                                <i class="glyphicon glyphicon-search" style="color:var(--bs-secondary)"></i>
-                            </button>
-                        </div>
+                        <input name="search" type="text" class="form-control rounded-pill" placeholder="Search">
+                    <div class="input-group-btn">
+                        <!-- <input class="btn btn-default" type="submit" name="submit" value="Submit"> -->
+                        <button class="btn btn-default" type="submit" name="submit">
+                            <i class="glyphicon glyphicon-search" style="black"></i>
+                        </button>
+                    </div>
                     </div>                    
                 </div>
             </div>
-            <div class="col gy-3">
+            <!-- <div class="col gy-3">
                 <div class="row gx-3">
                     <h6 class="p-2">Date:</h6>
                     <div class="input-group">
                         <input type="text" class="form-control rounded-pill" placeholder="Search" name="search">
-                        <div class="input-group-btn">
+                        <div class="input-group-btn"> -->
                             <!-- <button class="btn btn-default" type="submit">
                             <i class="glyphicon glyphicon-search" style="color:#58595d"></i>
                             </button> -->
-                        </div>
+                        <!-- </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 
-    <!-- Container that shows results from search -->
-    <div class="container-fluid bg-secondary darkContainer">
+    <!-- Container where the results will be displayed -->
+    <div class="container-fluid bg-secondary darkContainer h-75">
         <div class="container py-5 px-4 p-3 webWeeklyPlanTruckCard ">
             <div class="card my-1">
                 <div class="card-body p-5">
                     <div class="row">
                         <div class="col">
-                                <h3>Results</h1>
+                            <h3>Results</h1>
+                        </div>
+                        
+                        </div>
+                        <div class="row">
+                            <div class="col">                                  
+                        <?php if (isset($_POST['submit'])) {
+
+                        //global $conn;
+                        $search = mysqli_real_escape_string($conn, $_POST['search']);
+
+                        // Select all from completed jobs where the reference or order number is what was entered in the search input
+                        $sql = "SELECT completedjobs.completedJobID, completedjobs.completedJobName, completedjobs.completedJobDate, completedjobs.completedJobDestination, completedjobs.completedJobType, completedjobs.completedOrderNumber, completedjobs.completedReferenceNumber, completedjobs.completedPallets, completedjobs.completedJobWeight, completedjobs.completedJobStatus, completedjobs.completedJobDriverName_fk, customers.customerName, customers.customerSignature
+                                FROM completedJobs 
+                                INNER JOIN customers ON completedjobs.completedJobID = customers.completedJobID_fk
+                                WHERE completedReferenceNumber LIKE '%$search%' OR completedOrderNumber LIKE '%$search%'";
+                        $result = mysqli_query($conn, $sql);
+
+                        // Checking if anything matched the search value
+                        $queryResult = mysqli_num_rows($result);
+
+                        // Checking if theres more than 0 results
+                        if ($queryResult > 0) {
+
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "Job Name: " . $row['completedJobName'] . "<br>";
+                                echo "Date: " . $row['completedJobDate'] . "<br>";
+                                echo "Destination: " . $row['completedJobDestination'] . "<br>";
+                                echo "Type: " . $row['completedJobType'] . "<br>";
+                                echo "Order Number: " . $row['completedOrderNumber'] . "<br>";
+                                echo "Reference Number: " . $row['completedReferenceNumber'] . "<br>";
+                                echo "Pallets: " . $row['completedPallets'] . "<br>";
+                                echo "Weight: " . $row['completedJobWeight'] . "<br>";
+                                echo "Status: " . $row['completedJobStatus'] . "<br>";
+                                echo "Driver: " . $row['completedJobDriverName_fk'] . "<br>";
+                                echo "Customer Name: " . $row['customerName'] . "<br>";
+                                echo "Customer Signature: <br>";
+                                echo '<img src="data:image/jpeg;base64,'.base64_encode($row['customerSignature']) .'" />';
+                            }
+                        }
+                        else {
+                            // If there are no matches
+                            echo "No matching results. Please try again";
+                        }
+                        }; ?>
+
                         </div>
                         <div class="col d-flex flex-row-reverse">
-                                <button type="submit" name="submit" class="btn btn-primary text-light ">Export</button>
+                            <button type="submit" name="export" class="btn btn-primary text-light ">Export</button>
                         </div>
                     </div>
-                        <!-- <div class="row">
-                            <div class="col">                                    
-                            </div>
-                        </div> -->
                 </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
+</div>
+
 
 
 <!-- Bootstrap Bundle with Popper -->
