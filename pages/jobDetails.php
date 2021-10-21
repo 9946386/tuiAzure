05cc4d64d6d89a206b06e5dc6858c0df9c2d936a
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-10-21 17:18:59 +1300

    Customer details on completed job

--- pages/jobDetails.php
+++ pages/jobDetails.php
@@ -107,7 +107,7 @@ if (isset($_GET['id'])) {
         <!-- Button to complete the job which will then send the table entry from openjobs to completedjobs -->
         <div class="d-flex flex-row-reverse">
           <form action="" method="POST">
-            <button name="completed" type="submit" class="btn btn-primary text-light pt-4">Complete Job</button>
+            <button name="completed" type="submit" class="btn btn-primary text-light mt-4">Complete Job</button>
           </form>
         </div>
     </div> 
